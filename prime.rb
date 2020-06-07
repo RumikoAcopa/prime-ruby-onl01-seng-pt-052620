a66b77642349ac3b1ac93dfc8da9c6e2bc51047f
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 17:13:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,16 @@
-require 'prime'# Add  code here!
-def Prime.prime?(integer)
-  prime = 2,3,11,105557
-  non-prime = -1,0,1,4,40,1763,101013
-   if integer == prime
-     return true
-   else return false
-   end
-   prime
-  end
+require 'prime'
+def prime?(number)
+  (2..number/2).none? {|n| return false if number <= 1 || number % n == 0}
 end
+
+# Add  code here!
+#def Prime.prime?(integer)
+  #prime = 2,3,11,105557
+#  non-prime = -1,0,1,4,40,1763,101013
+   #if integer == prime
+    # return true
+   #else return false
+   #end
+  # prime
+  #end
+#end
