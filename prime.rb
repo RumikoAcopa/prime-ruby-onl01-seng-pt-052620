ae2822ede6f43332222498eb0d14f325c4b9411b
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 17:04:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 require 'prime'# Add  code here!
 def Prime.prime?(integer)
-  non-prime
-  return false if integer != true
+  prime = 2,3,11,105557 
+  non-prime = -1,0,1,4,40,1763,101013
+   if integer == prime 
+     return true 
+   else return false
+end
 end
